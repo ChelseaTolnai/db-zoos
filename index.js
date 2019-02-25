e4238b1ba8973093b50e3c0b733642c999f50a54
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-25 13:10:52 -0500

    Stretch: Created bears db table. Created CRUD endpoints for bears resource.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// *** ZOOS ENDPOINTS ***
 server.post('/api/zoos', async (req, res) => {
   try {
     const zoo = req.body;
@@ -93,6 +93,86 @@ server.delete('/api/zoos/:id', async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({error: "Error deleting specified zoo."})
+  }
+});
+
+// *** BEARS ENDPOINTS ***
+server.post('/api/bears', async (req, res) => {
+  try {
+    const bear = req.body;
+    if (bear.name) {
+      const [id] = await db('bears').insert(bear);
+      res.status(201).json(id)
+    } else {
+      res.status(400).json({error: "Bear name is required."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Error adding bear."})
+  }
+});
+
+server.get('/api/bears', async (req, res) => {
+  try {
+    const bears = await db('bears');
+    res.status(200).json(bears)
+  } catch (error) {
+    res.status(500).json({error: "Error getting bears."})
+  }
+});
+
+server.get('/api/bears/:id', async (req, res) => {
+  try {
+    const bear = await db('bears')
+      .where({ id: req.params.id })
+      .first();
+    if (bear) {
+      res.status(200).json(bear)
+    } else {
+      res.status(404).json({error: "Bear with specified ID does not exist."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Error getting specified bear."})
+  }
+});
+
+server.put('/api/bears/:id', async (req, res) => {
+  try {
+    const bear = req.body;
+    if (bear.name) {
+      const count = await db('bears')
+      .where({ id: req.params.id })
+      .update(bear);
+
+      if (count > 0) {
+        const updatedBear = await db('bears')
+        .where({ id: req.params.id })
+        .first();
+
+        res.status(200).json(updatedBear)
+      } else {
+        res.status(404).json({error: "Bear with specified ID does not exist."})
+      }
+    } else {
+      res.status(400).json({error: "Bear name is required."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Error updating specified bear."})
+  }
+});
+
+server.delete('/api/bears/:id', async (req, res) => {
+  try {
+    const count = await db('bears')
+    .where({ id: req.params.id })
+    .del();
+
+    if (count > 0) {
+      res.status(204).end()
+    } else {
+      res.status(404).json({error: "Bear with specified ID does not exist."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Error deleting specified bear."})
   }
 });
 
