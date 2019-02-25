7fcf579a5822b15deb09e0fffb10ae93052e8ae6
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-25 12:39:08 -0500

    Configure knex to connect to sqlite db. Created post endpoint.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3',
+  },
+  useNullAsDefault: true,
+}
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +17,21 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const zoo = req.body;
+    if (zoo.name) {
+      const [id] = await db('zoos').insert(zoo);
+
+      res.status(201).json(id)
+    } else {
+      res.status(400).json({error: "Zoo name is required."})
+    }
+
+  } catch (error) {
+    res.status(500).json({error: "Error adding zoo."})
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
